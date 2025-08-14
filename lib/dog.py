658bb01e740a7985fff173f1c45feaca66326346
nimrod kipngetich <nimrod.kipngetich@student.moringaschool.com>
2025-08-14 18:48:22 +0300

    update all the latest commits

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def bark(self):
+        """Make the dog bark."""
+        print("Woof!")
+    def sit(self):
+        """Make the dog sit."""
+        print("The dog is sitting.")
     # Class body goes here
-
+fido = Dog()  # Create an instance of Dog
+fido.bark()  # Call the bark method
     #Instance method definition
-    pass
+    
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
+    def talk(self):
+        """Make the person talk."""
+        print("Hello World!")
+    def walk(self):
+        """Make the person walk."""
+        print("The person is walking.")
     # Class body goes here
 
     #Instance method definition
